d3254a8e14add8c4416a37c4a7a5a7285ec01b7d
Neusj <52982808+Neusj@users.noreply.github.com>
2020-07-04 17:48:29 -0400

    se crearon las clases Accidente, Gravedad, Persona.

---
+++ evaluacionMod4/src/modelo/Gravedad.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Gravedad {
+	
+	private int id_gravedad;
+	private String nivel;
+	
+	
+	public Gravedad(int id_gravedad, String nivel) {
+		super();
+		this.id_gravedad = id_gravedad;
+		this.nivel = nivel;
+	}
+
+
+	public int getId_gravedad() {
+		return id_gravedad;
+	}
+
+
+	public void setId_gravedad(int id_gravedad) {
+		this.id_gravedad = id_gravedad;
+	}
+
+
+	public String getNivel() {
+		return nivel;
+	}
+
+
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+	
+	
+
+}
